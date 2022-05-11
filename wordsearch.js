ffe563afafe887567f0e6c550e541a1bb4ad869c
Heather <hmcleod29@gmail.com>
2022-05-11 19:33:07 -0400

    Solved the word search in pair programming with Lance Yu, using transpose and matrix functions from yesterdays pair programming (allowed as per the instructions) and the function given for horizontalJoin.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const printMatrix = (matrix) => {
+    for (const row of matrix) {
+        for (const el of row) {
+            process.stdout.write(el + " ");
+        }
+        process.stdout.write('\n');
     }
-}
+};
 
-module.exports = wordSearch
+
+const transpose = function(matrix) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    let ans = [];
+    for (var i = 0; i < cols; i++) { // ! i => row
+        ans[i] = [];
+        for (var j = 0; j < rows; j++) { // ! j => col
+            ans[i][j] = matrix[j][i];
+        }
+    };
+    return ans;
+};
+
+
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    console.log(horizontalJoin);
+    for (l of horizontalJoin) {
+        if (l.includes(word))
+            return true;
+    };
+
+    const transposedWords = transpose(letters);
+
+    const verticalJoin = transposedWords.map(ls => ls.join(''));
+    console.log(verticalJoin);
+    for (l of verticalJoin) {
+        if (l.includes(word))
+            return true;
+    };
+    return false;
+};
+
+module.exports = wordSearch;
